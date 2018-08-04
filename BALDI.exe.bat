d581825df61b58e05f658b6478ac4653565c0414
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-08-03 20:51:00 -0700

    Create BALDI.exe.bat

---
+++ BALDI.exe.bat
@@ -0,0 +1,26 @@
+@echo off
+start unity.bat
+title Baldi's Basics In Education AND Learning
+echo Baldi's Basics In Education AND Learning
+timeout 3
+echo THAT'S ME!
+pause
+cls
+pause
+start https://www.google.com/search?safe=active&ei=5CBlW9bkNIzKjwTcw5PABw&q=google+images+lol&oq=google+images+lol&gs_l=psy-ab.3..0j0i22i30k1l2.14379.18750.0.19191.17.14.0.3.3.0.147.1401.7j6.13.0....0...1c.1.64.psy-ab..1.16.1428...0i131i67k1j0i131k1j0i67k1.0.vPlfEZcv820
+echo   /
+echo ('-')
+echo /[]\
+echo / \
+timeout 60
+start https://www.google.com/search?q=.&oq=.&aqs=chrome..69i57j69i59l3j69i60j69i59&sourceid=chrome&ie=UTF-8
+timeout 3
+start https://scratch.mit.edu/projects/11752596/
+echo hahahhahah i was joking
+goto lol
+:lol
+echo   /
+echo ('-')
+echo /[]\
+echo / \
+goto lol
